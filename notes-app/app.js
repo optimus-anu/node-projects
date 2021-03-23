ad4fdd66e521202a5bc56c42b841bd0c8fe59116
optimus-anu <sanurag2291@gmail.com>
2021-03-23 20:13:45 +0530

    Lesson: Getting Input from Users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,13 @@
 const chalk = require('chalk');
 const getNotes = require('./notes')
 
-console.log(getNotes());
+const command = process.argv[2];
 
-console.log(chalk.green.inverse.bold('Success'));
+console.log(process.argv);
+
+if(command === 'add') {
+    console.log('Adding note!');
+} else if(command === 'remove') {
+    console.log('Removing note!');
+}
 
