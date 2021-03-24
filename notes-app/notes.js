a689b349a1551a3dff996ab89378bf8158b888e4
optimus-anu <sanurag2291@gmail.com>
2021-03-24 20:28:15 +0530

    Lesson: Adding a note

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,40 @@
+const fs = require('fs');
 const getNotes = function () {
     return "Your notes...";
 }
+const addNote = function (title, body) {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title;
+    });
+    if(duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        });
+        console.log('New Note Added!');
+    } else {
+        console.log('Title already present');
+    }
+    
+    saveNotes(notes);
+}
 
-module.exports = getNotes;
+const saveNotes = function(notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = function () {
+    try {
+        const dataJSON = fs.readFileSync('notes.json').toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    } 
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+};
