719dc9db4badf86d9692fa6ab9543ce81ae45e9f
optimus-anu <sanurag2291@gmail.com>
2021-03-27 16:25:09 +0530

    Lesson: Making HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,6 @@
-console.log('starting');
-
-setTimeout(() => {
-    console.log('2s timer');
-}, 2000);
-
-setTimeout(() => {
-    console.log('0s timer');
-}, 0);
-
-
-console.log('Stopping');
+const request = require('request');
+const url = 'http://api.weatherstack.com/current?access_key=62f0016857177d8b0387e798b22ae563&query=37.8267,-122.4233';
+request({url: url}, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+});
