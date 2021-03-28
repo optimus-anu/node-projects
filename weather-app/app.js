f6d7c91b94b39b96e5bd954b544110161d86c672
optimus-anu <sanurag2291@gmail.com>
2021-03-28 14:04:58 +0530

    Lesson: Callback Chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,24 @@
-const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Mathura', (error, data) => {
-    if(data) {
-        forecast(data.latitude, data.longitude, (error, response) => {
-            if(error) {
-                console.log(error);
-            } else {
-                console.log(`${response.body.location.name}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out`);
-            }
-        });  
-    } else {
-        console.log(error);
-    } 
-});
+if(process.argv[2]) {
+    geocode(process.argv[2], (error, data) => {
+        if (error) {
+            return console.log(error);
+        } else {
+            forecast(data.latitude, data.longitude, (error, response) => {
+                if(error) {
+                    return console.log(error);
+                } else {
+                    console.log(`${data.location}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out`);
+                }
+            }); 
+        }
+    });
+} else {
+    console.log('Please provide a location');
+}
+
 
 
     
