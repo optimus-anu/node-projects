92a020238dcf5a8da09e0a924ffd5d5becb70f0d
optimus-anu <sanurag2291@gmail.com>
2021-03-28 11:25:41 +0530

    Lesson: Callback Abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,31 @@
 const request = require('request');
-const url = 'http://api.weatherstack.com/current?access_key=62f0016857177d8b0387e798b22ae563&query=37.8267,-122.4233&units=f';
-request({url: url, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service');
-    } else if(response.body.error){
-        console.log('Unable to find location');
-    } else {
-        console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out`);
-    }
-});
+const geocode = require('./utils/geocode');
+// const url = 'http://api.weatherstack.com/current?access_key=62f0016857177d8b0387e798b22ae563&query=37.8267,-122.4233&units=f';
+// request({url: url, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service');
+//     } else if(response.body.error){
+//         console.log('Unable to find location');
+//     } else {
+//         console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out`);
+//     }
+// });
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoib3B0aW11cy1hbnUiLCJhIjoiY2ttcnA5ajhuMDlsNDJxczlxcXExb21qMiJ9.3TOmgbGFgu-9hDk4Nih2WA';
-request({url: geocodeURL, json: true}, (error, response) => {
-    if(error) {
-        console.log('Unable to connect to location services!');
-    } else if(response.body.features.length === 0) {
-        console.log('Unable to find the location. Try another search.');
-    } else {
-        const latitude = response.body.features[0].center[1];
-        const longitude = response.body.features[0].center[0];
-        console.log(`Latitude: ${latitude} Longitude: ${longitude}`);
-    }
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoib3B0aW11cy1hbnUiLCJhIjoiY2ttcnA5ajhuMDlsNDJxczlxcXExb21qMiJ9.3TOmgbGFgu-9hDk4Nih2WA';
+// request({url: geocodeURL, json: true}, (error, response) => {
+//     if(error) {
+//         console.log('Unable to connect to location services!');
+//     } else if(response.body.features.length === 0) {
+//         console.log('Unable to find the location. Try another search.');
+//     } else {
+//         const latitude = response.body.features[0].center[1];
+//         const longitude = response.body.features[0].center[0];
+//         console.log(`Latitude: ${latitude} Longitude: ${longitude}`);
+//     }
+// });
+
+geocode('Mathura', (error, data) => {
+    console.log('Error',error);
+    if(data)
+    console.log('Data', data);
 });
