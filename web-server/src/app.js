1433b169c0106bfb49a7220319e16b17bb61791d
optimus-anu <sanurag2291@gmail.com>
2021-03-29 14:21:01 +0530

    Lesson: Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,30 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello express!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help Page!');
+    res.send([{
+        name: 'Anurag',
+        age: 22
+    },
+    {
+        name: "Abhishek",
+        age: 23,
+    }
+]);
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.send('<h2>This is about page</h2>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Show Weather!');
+    res.send({
+        place: "Mathura",
+        weather: "Clear Sunny",
+    });
 });
 
 app.listen(3000, () => {
