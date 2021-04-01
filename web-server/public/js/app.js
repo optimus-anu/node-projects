5fc88ebb20358206dd3be6fcde7f66cabd1bc30f
optimus-anu <sanurag2291@gmail.com>
2021-04-01 11:24:11 +0530

    Lesson: Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side javaScript file is loaded');
+
+fetch('http://localhost:3000/weather?address=mathura').then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            console.log(data.error);
+        }else {
+            console.log(`Location: ${data.location}`);
+            console.log(`Current Temperature ${data.currentTemperature}`);
+        }
+    })
+})
