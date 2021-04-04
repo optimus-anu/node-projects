7396a00fd78606750a633ab7830056ed3e4c5fbf
optimus-anu <sanurag2291@gmail.com>
2021-04-04 10:15:07 +0530

    Lesson: Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,8 +13,51 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Anurag',
-        age: 27
-    });
+    // db.collection('users').insertOne({
+    //     name: 'Anurag',
+    //     age: 27
+    // }, (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert user');
+    //     }
+
+    //     console.log(result.ops);
+    // });
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert documents !');
+    //     }
+
+    //     console.log(result.ops);
+    // })
+
+    db.collection('users').insertMany([
+        {
+            description: 'clean the house',
+            completed: true
+        },
+        {
+            description: 'Renew Inspection',
+            completed: false,
+        },
+        {
+            description: 'Pot plants',
+            completed: false,
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log('Unable to insert the documents !');
+        }
+        console.log(result.ops);
+    })
 });
