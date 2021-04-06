17e30ef53bdaf053ac1fcc1babde4531d14aa882
optimus-anu <sanurag2291@gmail.com>
2021-04-06 22:28:35 +0530

    Lesson: Promises

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -1,32 +1,13 @@
-setTimeout(() => {
-    console.log('Two seconds are up');
-}, 2000);
-
-const names = ['Andrew', 'Jen', 'Jess'];
-const shortNames = names.filter((name) => {
-    return name.length <= 4;
-});
-
-const geocode = (address, callback) => {
+const doWorkCallback = (callback) => {
     setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
-        callback(data);
-    }, 2000);
-}
-geocode('Philadelphia', (data) => {
-    console.log(data);
-});
-
-const add = (a, b, callback) => {
-    setTimeout(() => {
-        const sum = a + b;
-        callback(sum);
-    }, 2000);
+        // callback('This is my error', undefined);
+        callback(undefined, [1,4,7])
+    }, 2000)
 }
 
-add(1, 4, (sum) => {
-    console.log(sum);
-})
+doWorkCallback((error, result) => {
+    if(error) {
+        return console.log(error);
+    }
+    console.log(result);
+});
