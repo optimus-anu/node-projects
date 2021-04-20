8b3a185038bc524b08bf6d3a410bfb5ee1057629
optimus-anu <sanurag2291@gmail.com>
2021-04-20 09:19:18 +0530

    Lesson: Accepting Authentication Tokens

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,18 +9,6 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if(req.method === "GET") {
-//         res.send('GET Requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-app.use((req, res, next) => {
-    res.status(503).send('Site is under maintainance')
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
