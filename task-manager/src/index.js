e2f0d0c768df9d2b7a0f2c12c7aa8fab6c7801e2
optimus-anu <sanurag2291@gmail.com>
2021-04-21 07:22:28 +0530

    Lesson: Hiding Private Data

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -17,13 +17,3 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
 
-const jwt = require('jsonwebtoken')
-const myFunction = async () => {
-    const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse', {expiresIn: '7 days'})
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
-
-myFunction()
