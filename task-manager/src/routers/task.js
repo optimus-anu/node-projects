c8966edeea9ca839c01d94d8cb096cd3f3392be6
optimus-anu <sanurag2291@gmail.com>
2021-04-27 18:11:02 +0530

    Lesson: Paginating Data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -19,6 +19,7 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 //GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
 router.get('/tasks',auth, async (req, res) => {
     const match = {}
     if(req.query.completed) {
@@ -28,7 +29,11 @@ router.get('/tasks',auth, async (req, res) => {
         // const tasks = await Task.find({owner: req.user._id})
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch(e) {
